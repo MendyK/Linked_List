0f915228a63eb8776f7ed7cee979e84aa26d72c4
Mendy Krinsky <mendyk@Mendys-MBP.home>
2017-03-26 21:14:00 -0400

    Documentation

--- linkedlist.h
+++ linkedlist.h
@@ -1,6 +1,10 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+/**
+Only strings can be added to this list at the moment.
+*/
+
 typedef struct node {
 	void *word;
 	struct node *next;
@@ -10,13 +14,12 @@ void print_list(node_t **head);
 
 /**
 Appends a node to the node of the list.
-Returns -1 if you tried to insert a duplicate, does not add it to the list
+Returns a negative value if you try to insert a duplicate, then does not add it to the list.
 */
 int append_node(node_t **head, void *word);
 
 /**
-Removes all nodes from the list and frees all memory/
-Returns a negative value if unsuccessful
+Removes all nodes from the list and frees all memory.
 */
 void remove_all(node_t **head);
 
@@ -28,7 +31,7 @@ node_t *node_at_index(node_t **head, int index);
 
 /**
 Index of given node_t in list.
-Returns a negative value if the node is not in the list.
+Returns a negative value if 'node' is not in the list.
 */
 int index_of_node(node_t **head, node_t *node);
 
